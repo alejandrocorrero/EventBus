ac33dc20adb8cdbb6c153de7452036c81c7dda2a
alex <alejandro.correro@hotmail.com>
2018-01-25 23:33:13 +0100

    comentado

---
+++ app/src/main/java/com/example/usuario/broadcastejemplo/ProgresoEvento.java
@@ -0,0 +1,13 @@
+package com.example.usuario.broadcastejemplo;
+
+public class ProgresoEvento {
+    private final int stepNumber;
+
+    public ProgresoEvento(int stepNumber) {
+        this.stepNumber = stepNumber;
+    }
+
+    public int getStepNumber() {
+        return stepNumber;
+    }
+}
